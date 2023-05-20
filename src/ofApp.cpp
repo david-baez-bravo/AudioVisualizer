9eece355e02082bf3c6bd3d88a687e6c352b5473
Baez-Bravo-David <davbaezbravo@outlook.com>
2023-05-20 19:52:41 -0400

    Initial commit

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -4,23 +4,45 @@
 void ofApp::setup() {
     sound.load("beat.wav");           // Loads a sound file (in bin/data/)
     sound.setLoop(true);              // Makes the song loop indefinitely
-    sound.setVolume(1);               // Sets the song volume
+    sound.setVolume(1);               // Sets the song volume 
 }
 
 //--------------------------------------------------------------
 void ofApp::update() {
     /* The update method is called muliple times per second
     It's in charge of updating variables and the logic of our app */
-    ofSoundUpdate();                    // Updates all sound players
-    if (!paused){                       // Pauses Visualizer
-        visualizer.updateAmplitudes();  // Updates Amplitudes for visualizer
+    ofSoundUpdate();               // Updates all sound players
+    if (!paused) {
+        visualizer.updateAmplitudes(); // Updates Amplitudes for visualizer
     }
     progress = sound.getPosition();
+
+    // loop
+    if (loop && progress >= .99) {
+        if (song_index < (int)(songs.size() - 1)) {
+            changeSong(songs[song_index + 1]);
+            song_index++;
+        } else {
+            song_index = 0;
+            changeSong(songs[song_index]);
+        }
+    }
+
+    // repeat
+    if (repeat && progress >= .99) {
+        changeSong(songs[song_index]);
+    }
+
+    //shuffle
+    if (shuffle) {
+        changeSong(songs[ofRandom(songs.size())]);
+        shuffle = false;
+    }
 }
 
 //--------------------------------------------------------------
 void ofApp::draw() {
-    /* The draw method is called muliple times per second
+    /* The update method is called muliple times per second
     It's in charge of drawing all figures and text on screen */
 
     // Progress Bar
@@ -30,6 +52,7 @@ void ofApp::draw() {
     float pos = playing ? progress : lastPos;
     int percent = pos * 100;
     ofDrawBitmapString("Song Progress: " + ofToString(percent) + "%", 0, 30);
+    ofDrawRectangle(0,45,ofGetWidth()*pos,25); // progress bar
 
     // Mode Selection
     if (!playing) {
@@ -38,24 +61,69 @@ void ofApp::draw() {
     vector<float> amplitudes = visualizer.getAmplitudes();
     if (mode == '1') {
         drawMode1(amplitudes);
+        ofSetBackgroundColor(16, 50, 26); // Sets the Background Color
     } else if (mode == '2') {
         drawMode2(amplitudes);
+        ofSetBackgroundColor(36, 32, 56); // Sets the Background Color
     } else if (mode == '3') {
         drawMode3(amplitudes);
+        ofSetBackgroundColor(70, 22, 56); // Sets the Background Color
+    } else if (mode == '4') {
+        drawMode4(amplitudes);
+        ofSetBackgroundColor(170, 172, 0); // Sets the Background Color
     }
+
+    // adds visuals for modes
+    if (loop) {
+        ofSetColor(0, 0, 200);
+        ofDrawBitmapString("Loop", 0, 85);
+    } else {
+        ofSetColor(200, 0, 0);
+        ofDrawBitmapString("Loop", 0, 85);
+    }
+    if (repeat) {
+        ofSetColor(0, 0, 200);
+        ofDrawBitmapString("Repeat   (This is on by default)", 0, 100);
+    } else {
+        ofSetColor(200, 0, 0);
+        ofDrawBitmapString("Repeat   (This is on by default)", 0, 100);
+    }
+
+    // adds the keys and their uses as visuals
+    ofSetColor(256);
+    ofDrawBitmapString("[a] Pause", 0, ofGetHeight() - 90);
+    ofDrawBitmapString("[l] Loop", 0, ofGetHeight() - 75);
+    ofDrawBitmapString("[r] Repeat", 0, ofGetHeight() - 60);
+    ofDrawBitmapString("[b] Shuffle", 0, ofGetHeight() - 45);
+    ofDrawBitmapString("[!] Song 1", 150, ofGetHeight() - 90);
+    ofDrawBitmapString("[@] Song 2", 150, ofGetHeight() - 75);
+    ofDrawBitmapString("[#] Song 3", 150, ofGetHeight() - 60);
+    ofDrawBitmapString("[$] Song 4", 150, ofGetHeight() - 45);
+    ofDrawBitmapString("[%] Song 5", 150, ofGetHeight() - 30);
+    ofDrawBitmapString("[-] Volume Down", 300, ofGetHeight() - 90);
+    ofDrawBitmapString("[=] Volume Up", 300, ofGetHeight() - 75);
+    ofDrawBitmapString("[1] Mode 1", 450, ofGetHeight() - 90);
+    ofDrawBitmapString("[2] Mode 2", 450, ofGetHeight() - 75);
+    ofDrawBitmapString("[3] Mode 3", 450, ofGetHeight() - 60);
+    ofDrawBitmapString("Volume" + to_string(sound.getVolume()) + "%", 700, ofGetHeight() - 90);
+
 
     // ofDrawBitmapString("Current Mouse Position: " + ofToString(cur_x) + ", " + ofToString(cur_y), 0, 30);
 }
 void ofApp::drawMode1(vector<float> amplitudes) {
-    ofSetBackgroundColor(ofColor::dodgerBlue); // Sets the Background Color
     ofFill();        // Drawn Shapes will be filled in with color
     ofSetColor(256); // This resets the color of the "brush" to white
     ofDrawBitmapString("Rectangle Height Visualizer", 0, 15);
-    ofSetColor(0, 0, 200);
-    ofDrawRectRounded(2, ofGetHeight() - 100, 50, amplitudes[0], 10);
+    ofSetColor(0, 0, ofRandom(256));
+    float width_visual = ofGetWidth() / 64.0;
+    float position = 0.0; 
+    for (float amp : amplitudes){
+        ofDrawRectRounded(position,ofGetHeight()-100, width_visual, amp, 10);
+        position += width_visual;
+
+    }
 }
 void ofApp::drawMode2(vector<float> amplitudes) {
-    ofSetBackgroundColor(ofColor::seaGreen); // Sets the Background Color
     ofSetLineWidth(5); // Sets the line width
     ofNoFill();        // Only the outline of shapes will be drawn
     ofSetColor(256);   // This resets the color of the "brush" to white
@@ -68,10 +136,38 @@ void ofApp::drawMode2(vector<float> amplitudes) {
 }
 
 void ofApp::drawMode3(vector<float> amplitudes) {
-    ofSetBackgroundColor(ofColor::fireBrick); // Sets the Background Color
-    ofSetColor(256); // This resets the color of the "brush" to white
-    ofDrawBitmapString("Rectangle Width Visualizer", 0, 15);
+    ofSetColor(255,95,0); // This resets the color of the "brush" to white
+    ofDrawBitmapString("Curve Visualizer", 0, 15);
     // YOUR CODE HERE
+    float width_visual = ofGetWidth() / 64.0;
+    float position = 0.0; 
+    for (float amp : amplitudes){
+        ofDrawCurve(position, 20*amp, 0, ofGetHeight()/2, ofGetWidth(), ofGetHeight()/2, position, -20*amp);
+        position += width_visual;
+
+    }
+}
+
+void ofApp::drawMode4(vector<float> amplitudes) {
+    ofSetColor(0,195,0); // This resets the color of the "brush" to white
+    ofDrawBitmapString("Elipse Visualizer/Cool Visualizer", 0, 15);
+    // YOUR CODE HERE
+    float width_visual = ofGetWidth() / 64.0;
+    float position = 0.0; 
+    for (float amp : amplitudes){
+        ofDrawEllipse(ofGetWidth() / 2, ofGetHeight() / 2, position, amp);
+        position += width_visual;
+
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::changeSong(string filename){ 
+    sound.load(filename);
+    sound.play();
+    playing = true;
+    paused = false;
+    sound.setLoop(true);
 }
 
 //--------------------------------------------------------------
@@ -89,6 +185,42 @@ void ofApp::keyPressed(int key) {
     case 'a':
         paused = !paused;
         break;
+    case 'l':
+        loop = !loop;
+        if (repeat) {
+            repeat = !repeat;
+        }
+        break;
+    case 'r':
+        repeat = !repeat;
+        if (loop) {
+            loop = !loop;
+        }
+        break;
+    case 'b':
+        shuffle = !shuffle;
+        break;
+    case '!':
+        changeSong(songs[0]);
+        break;
+     case '@':
+        changeSong(songs[1]);
+        break; 
+    case '#':
+        changeSong(songs[2]);
+        break; 
+    case '$':
+        changeSong(songs[3]);
+        break; 
+    case '%':
+        changeSong(songs[4]);
+        break;
+    case '=':
+        sound.setVolume(min(sound.getVolume()+0.1,1.0));
+        break;
+    case '-':
+       sound.setVolume(max(sound.getVolume()-0.1,0.0));
+        break;
     case '1':
         mode = '1';
         break;
@@ -97,6 +229,9 @@ void ofApp::keyPressed(int key) {
         break;
     case '3':
         mode = '3';
+        break;
+    case '4':
+        mode = '4';
         break;
     }
 }
@@ -113,6 +248,11 @@ void ofApp::mouseMoved(int x, int y) {
 
 //--------------------------------------------------------------
 void ofApp::mouseDragged(int x, int y, int button) {
+    // if moused dragged and clicked at the progress bar then it will alter the progress
+    if ((y <= 70) && (y >= 45)) {
+        float newProgress = ofMap(x, 0, ofGetWidth(), 0, 1);
+        sound.setPosition(newProgress);
+    }
 }
 
 //--------------------------------------------------------------
--- src/ofApp.hpp
+++ src/ofApp.hpp
@@ -8,11 +8,18 @@ class ofApp : public ofBaseApp {
   private:
     ofSoundPlayer sound;
     AudioVisualizer visualizer;
+    vector<string>songs = {"beat.wav","geesebeat.wav","pigeon_coo.wav","rock-song.wav", "Metro_Boomin.wav"};
 
     bool playing = false;
     bool paused = false;
+    bool loop = false;
+    bool repeat = true;
+    bool shuffle = false;
     char mode = '1';
+    int song_index=0;
+    
 
+    
     int cur_x, cur_y = 0;
     float sound_length;
 
@@ -27,6 +34,9 @@ class ofApp : public ofBaseApp {
     void drawMode1(vector<float> amplitudes);
     void drawMode2(vector<float> amplitudes);
     void drawMode3(vector<float> amplitudes);
+    void drawMode4(vector<float> amplitudes);
+
+    void changeSong(string filename);
 
     void keyPressed(int key);
     void keyReleased(int key);
